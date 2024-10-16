d738da93f9875d3e0a935552973eb8fff63803d6
SolangePaniagua05 <125227513+solange05@users.noreply.github.com>
2024-10-16 16:59:20 -0300

    Delete src directory

--- src/app/models/alumno.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Alumno {
-    id: number;
-    nombre: string;
-    apellido: string;
-    curso: string;
-  }
--- src/app/nombre-completo.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NombreCompletoPipe } from './nombre-completo.pipe';
-
-describe('NombreCompletoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NombreCompletoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
