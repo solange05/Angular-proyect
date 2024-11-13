1346aab73e8613b407f421181f682a599b39bac0
SolangePaniagua05 <125227513+solange05@users.noreply.github.com>
2024-11-13 16:38:52 -0300

    Delete src directory

--- src/app/tamanio-cabecera.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TamanioCabeceraDirective } from './tamanio-cabecera.directive';
-
-describe('TamanioCabeceraDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TamanioCabeceraDirective();
-    expect(directive).toBeTruthy();
-  });
-});
